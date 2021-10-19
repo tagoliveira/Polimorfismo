afa41939c8dc29c92077548f6360df23a4517669
tagoliveira <tagoliveira.dev@gmail.com>
2021-10-19 10:38:33 -0300

    Add project files.

---
+++ Polimorfismo1/Pessoa.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polimorfismo1
+{
+    class Pessoa
+    {
+        private string nome;
+        private string endereco;
+
+        public void gravar(string nome, string endereco)
+        {
+            this.nome = nome;
+            this.endereco = endereco;
+        }
+
+        public string getNome()
+        {
+            return nome;
+        }
+
+        public string getEndereco()
+        {
+            return endereco;
+        }
+    }
+}
---
+++ Polimorfismo1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Polimorfismo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PessoaFisica obj_pf = new PessoaFisica();
+            obj_pf.gravar("Thiago", "Rua", "adsf", "2342342", 34);
+            Console.WriteLine(obj_pf.getNome());
+            Console.WriteLine(obj_pf.getEndereco());
+            Console.WriteLine(obj_pf.getApelido());
+            Console.WriteLine(obj_pf.getCpf());
+            Console.WriteLine(obj_pf.getIdade());
+        }
+    }
+}
